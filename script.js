ea08950833200fd153db3bfead78bfc50d2d631e
Timothy Rehenel <timothy.rehenel@gmail.com>
2025-03-20 10:04:04 +0200

    added forms section

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hi")
+function alertForm(e) {
+    e.preventDefault();
+    let result = "";
+    for (const input of e.target) {
+        if (input.type == "submit") {
+            continue;
+        }
+
+        result += `${input.name} = ${input.value} `;
+    }
+    alert(result);
+}
+
+document.querySelector("form.login").addEventListener("submit", alertForm)
+document.querySelector("form.subscribe").addEventListener("submit", alertForm)
